ac45d5cd440a2f5992a2f78ca60c1abd8a6c3e2d
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-02 12:27:27 -0400

    Complete Day 16 - Project 1, part one

---
+++ 01-Project-1-StormViewer/StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Matt X on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // This is a picture to load...
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+}
+
