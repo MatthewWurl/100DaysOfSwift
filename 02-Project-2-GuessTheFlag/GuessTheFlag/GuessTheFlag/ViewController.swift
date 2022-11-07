d502e9c21f79f8cca7b62caa84c6e13dfaf4a1fc
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-07 14:30:26 -0500

    Complete Day 19 - Project 2, part one

---
+++ 02-Project-2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Matt X on 11/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries: [String] = []
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries = ["estonia", "france", "germany", "ireland",
+                     "italty", "monaco", "nigeria", "poland",
+                     "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+}
