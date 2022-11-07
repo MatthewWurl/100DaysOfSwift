8535e2d81bd708245d6bea4b6c7d92919af3a570
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-07 14:49:31 -0500

    Complete Day 20 - Project 2, part two

--- 02-Project-2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ViewController.swift
+++ 02-Project-2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries: [String] = []
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +34,39 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Incorrect!"
+            score -= 1
+        }
+        
+        let alertController = UIAlertController(
+            title: title,
+            message: "Your score is now \(score).",
+            preferredStyle: .alert
+        )
+        
+        alertController.addAction(
+            UIAlertAction(title: "Continue", style: .default, handler: askQuestion)
+        )
+        
+        present(alertController, animated: true)
     }
 }
