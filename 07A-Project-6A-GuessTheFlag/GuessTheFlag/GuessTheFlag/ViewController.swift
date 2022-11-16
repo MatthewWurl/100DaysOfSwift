1635da1405dea5aacf21e666280d98b2d45a77b4
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-16 00:03:45 -0500

    Complete Day 30 - Project 6, part one
    Includes two projects: part A and part B

---
+++ 07A-Project-6A-GuessTheFlag/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Matt X on 11/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries: [String] = []
+    var score = 0
+    var correctAnswer = 0
+    var questionCount = 0
+    
+    let NUMBER_OF_QUESTIONS = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries = ["estonia", "france", "germany", "ireland",
+                     "italy", "monaco", "nigeria", "poland",
+                     "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+
+    func askQuestion(action: UIAlertAction! = nil) {
+        if questionCount == NUMBER_OF_QUESTIONS {
+            let gameOverAlert = UIAlertController(
+                title: "Game Over!",
+                message: "You answered all \(NUMBER_OF_QUESTIONS) questions!\nYour score was \(score)/\(NUMBER_OF_QUESTIONS).",
+                preferredStyle: .alert
+            )
+            
+            gameOverAlert.addAction(
+                UIAlertAction(title: "Play Again", style: .default, handler: resetGame)
+            )
+            
+            present(gameOverAlert, animated: true)
+            
+            return
+        }
+        
+        questionCount += 1
+        
+        countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        let correctCountryAsTitle = countries[correctAnswer].uppercased()
+        title = "\(correctCountryAsTitle) - Score: \(score)"
+    }
+    
+    func resetGame(action: UIAlertAction! = nil) {
+        score = 0
+        questionCount = 0
+        
+        askQuestion()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message = ""
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Incorrect!"
+            message += "Sorry, that's the flag of \(countries[sender.tag].uppercased())!\n"
+            score -= 1
+        }
+        
+        message += "Your score is now \(score)."
+        
+        let scoreAlert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        scoreAlert.addAction(
+            UIAlertAction(title: "Continue", style: .default, handler: askQuestion)
+        )
+        
+        present(scoreAlert, animated: true)
+    }
+}
