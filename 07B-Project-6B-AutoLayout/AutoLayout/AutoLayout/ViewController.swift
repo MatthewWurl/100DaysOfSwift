d9a0d499cb05f5d7961b0bda52d5ef667d7af2f4
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-16 21:04:51 -0500

    Complete Day 31 - Project 6, part two

--- 07B-Project-6B-AutoLayout/AutoLayout/AutoLayout/ViewController.swift
+++ 07B-Project-6B-AutoLayout/AutoLayout/AutoLayout/ViewController.swift
@@ -47,31 +47,52 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = [
-            "label1": label1,
-            "label2": label2,
-            "label3": label3,
-            "label4": label4,
-            "label5": label5
-        ]
+//        let viewsDictionary = [
+//            "label1": label1,
+//            "label2": label2,
+//            "label3": label3,
+//            "label4": label4,
+//            "label5": label5
+//        ]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(
+//                NSLayoutConstraint.constraints(
+//                    withVisualFormat: "H:|[\(label)]|",
+//                    metrics: nil,
+//                    views: viewsDictionary
+//                )
+//            )
+//        }
+//
+//        let metrics = [
+//            "labelHeight": 88
+//        ]
+//
+//        view.addConstraints(
+//            NSLayoutConstraint.constraints(
+//                withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|",
+//                metrics: metrics,
+//                views: viewsDictionary
+//            )
+//        )
         
-        for label in viewsDictionary.keys {
-            view.addConstraints(
-                NSLayoutConstraint.constraints(
-                    withVisualFormat: "H:|[\(label)]|",
-                    metrics: nil,
-                    views: viewsDictionary
-                )
-            )
+        let labels = [label1, label2, label3, label4, label5]
+        var previous: UILabel?
+        
+        for label in labels {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
         }
-        
-        view.addConstraints(
-            NSLayoutConstraint.constraints(
-                withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]",
-                metrics: nil,
-                views: viewsDictionary
-            )
-        )
     }
 }
 
