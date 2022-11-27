6c96305bb5434262ee4187b30d58bc9231597843
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-27 01:18:50 -0500

    Complete Day 42 - Project 10, part one

---
+++ 13-Project-10-NamesToFaces/NamesToFaces/NamesToFaces/CollectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionViewController.swift
+//  NamesToFaces
+//
+//  Created by Matt X on 11/27/22.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        return cell
+    }
+}
