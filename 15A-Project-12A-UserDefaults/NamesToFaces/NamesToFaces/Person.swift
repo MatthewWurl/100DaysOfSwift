bac9c599790fe632d03eb058f820ad33e2870832
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-12-03 00:22:47 -0500

    Add base project for Project 15A

---
+++ 15A-Project-12A-UserDefaults/NamesToFaces/NamesToFaces/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Matt X on 11/28/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    static let defaultName = "Unknown"
+    
+    var name: String
+    let image: String
+    
+    init(name: String = defaultName, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ 15A-Project-12A-UserDefaults/NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Matt X on 11/27/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
