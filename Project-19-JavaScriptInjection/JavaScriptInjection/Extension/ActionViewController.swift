7c9d1c60891c9d1404bdbae6fc76ee7c2447a2e9
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-12-22 02:26:42 -0500

    Complete Day 67 - Project 19, part one

---
+++ Project-19-JavaScriptInjection/JavaScriptInjection/Extension/ActionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Matt X on 12/21/22.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] dict, error in
+                    
+                }
+            }
+        }
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
---
+++ Project-19-JavaScriptInjection/JavaScriptInjection/JavaScriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaScriptInjection
+//
+//  Created by Matt X on 12/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
