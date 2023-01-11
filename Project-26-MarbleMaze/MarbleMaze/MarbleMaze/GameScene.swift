7524070704c990ce400ff3cf6199752a6306eaf4
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2023-01-10 21:12:25 -0500

    Complete Day 86 - Project 26, part two

--- Project-26-MarbleMaze/MarbleMaze/MarbleMaze/GameScene.swift
+++ Project-26-MarbleMaze/MarbleMaze/MarbleMaze/GameScene.swift
@@ -5,9 +5,10 @@
 //  Created by Matt X on 1/9/23.
 //
 
+import CoreMotion
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     enum CollisionTypes: UInt32 {
         case player = 1
         case wall = 2
@@ -16,6 +17,17 @@ class GameScene: SKScene {
         case finish = 16
     }
     
+    var isGameOver = false
+    var lastTouchPosition: CGPoint?
+    var motionManager: CMMotionManager?
+    var player: SKSpriteNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var scoreLabel: SKLabelNode!
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -23,7 +35,20 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
+        
         loadLevel()
+        createPlayer()
+        
+        physicsWorld.contactDelegate = self
+        
+        motionManager = CMMotionManager()
+        motionManager?.startAccelerometerUpdates()
     }
     
     func loadLevel() {
@@ -97,6 +122,93 @@ class GameScene: SKScene {
                     fatalError("Unknown level letter found: \(letter)")
                 }
             }
+        }
+    }
+    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672) // Exact position for level 1...
+        player.zPosition = 1
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        
+        addChild(player)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        guard !isGameOver else { return }
+        
+        #if targetEnvironment(simulator) // Use touch for simulator...
+        if let lastTouchPosition = lastTouchPosition {
+            let xPosDiff = lastTouchPosition.x - player.position.x
+            let yPosDiff = lastTouchPosition.y - player.position.y
+            let diff = CGPoint(x: xPosDiff, y: yPosDiff)
+            
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
+        #else // Use tilt information for real device...
+        if let accelerometerData = motionManager?.accelerometerData {
+            // dx and dy are flipped because the device is rotated by default...
+            let dx = accelerometerData.acceleration.y
+            let dy = accelerometerData.acceleration.x
+            physicsWorld.gravity = CGVector(dx: dx * -50, dy: dy * 50)
+        }
+        #endif
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerCollided(with: nodeB)
+        } else if nodeB == player {
+            playerCollided(with: nodeA)
+        }
+    }
+    
+    func playerCollided(with node: SKNode) {
+        if node.name == "vortex" {
+            player.physicsBody?.isDynamic = false
+            isGameOver = true
+            score -= 1
+            
+            let move = SKAction.move(to: node.position, duration: 0.25)
+            let scale = SKAction.scale(to: 0.0001, duration: 0.25)
+            let remove = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([move, scale, remove])
+            
+            player.run(sequence) { [weak self] in
+                self?.createPlayer()
+                self?.isGameOver = false
+                self?.physicsWorld.gravity = .zero // Reset gravity...
+            }
+        } else if node.name == "star" {
+            node.removeFromParent()
+            score += 1
+        } else if node.name == "finish" {
+            // Go to next level...
         }
     }
 }
